d4c9904afd05fc78bd0b73823d4eb582b53697e2
robinp7720 <robin@decker.cx>
2016-07-17 11:47:18 +0200

    Improved themes

--- .config/i3/themer/__init__.py
+++ .config/i3/themer/__init__.py
@@ -20,7 +20,6 @@ config.read(i3themerpath+'themes/'+theme+'.ini')
 def renderi3(window):
     template = open(i3themerpath+'theme.template').read()
     for key in config[window]:
-        print(key, config[window][key])
         template = template.replace(key, config[window][key])
     outputConf = open(i3path+'config', 'w')
     outputConf.truncate()
@@ -29,7 +28,6 @@ def renderi3(window):
 def renderi3blocks(window):
     template = open(i3themerpath+'i3blocks.template').read()
     for key in config[window]:
-        print(key, config[window][key])
         template = template.replace(key, config[window][key])
     outputConf = open(i3path+'i3blocks.conf', 'w')
     outputConf.truncate()
@@ -41,9 +39,29 @@ def render(window):
     renderi3blocks(window)
     call(["i3-msg", "reload"])
 
-print(wmclass[1])
-if wmclass[1] in config:
-    render(wmclass[1])
-else:
-    render("default")
+status = open(i3themerpath+'.status','r')
+statstext = status.read()
 
+if wmclass is None:
+    if statstext != "desktop":
+        render("desktop")
+        status = open(i3themerpath+'.status','w')
+        status.truncate()
+        status.write("desktop")
+        print('Set new theme')
+else:
+    print(wmclass[1])
+    if wmclass[1] in config:
+        if statstext != wmclass[1]:
+            render(wmclass[1])
+            status = open(i3themerpath+'.status','w')
+            status.truncate()
+            status.write(wmclass[1])
+            print('Set new theme')
+    else:
+        if statstext != "default":
+            render("default")
+            status = open(i3themerpath+'.status','w')
+            status.truncate()
+            status.write("default")
+            print('Set new theme')
