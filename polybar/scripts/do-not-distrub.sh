421f5bd500a4e46cac81cc24c971633f3b54bb1e
Zeyphros <robin@decker.cx>
2022-02-07 17:20:22 +0100

    Keep color for active DNT in a variable

--- polybar/scripts/do-not-distrub.sh
+++ polybar/scripts/do-not-distrub.sh
@@ -2,25 +2,27 @@
 
 notifications_enabled_icon="/usr/share/icons/Adwaita/64x64/apps/preferences-system-notifications-symbolic.symbolic.png"
 
+active_color="#FF3333"
+
+
 trap "toggle" USR1
 
 update() {
 	if [[ "$(dunstctl is-paused)" == "false" ]]; then
 		echo ""
 	else
-		echo "%{F#FF3333}ﮗ"
+		echo "%{F$active_color}ﮗ"
 	fi
 }
 
 
 toggle() {
-
 	if [[ "$(dunstctl is-paused)" == "true" ]]; then
 		echo ""
 		dunstctl set-paused toggle
 		dunstify -i "$notifications_enabled_icon" "Notifications" "Enabled"
 	else
-		echo "%{F#FF3333}ﮗ"
+		echo "%{F$active_color}ﮗ"
 		dunstify -i "$notifications_enabled_icon" "Notifications" "Disabled"
 		sleep 1
 		dunstctl close
