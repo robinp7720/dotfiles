b645f5041b9475388919a5d7795136b051bc0936
Robin Decker <robin@decker.cx>
2025-11-04 14:53:39 +0100

    Respect AURHELPER env var in update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,35 @@
-yay -Syu --noconfirm --devel --timeupdate
+#!/usr/bin/env bash
+
+set -uo pipefail
+
+helper="${AURHELPER:-paru}"
+
+case "$helper" in
+  paru)
+    cmd=(paru -Syu --noconfirm --sudoloop --devel --pgpfetch)
+    ;;
+  yay)
+    cmd=(yay -Syu --noconfirm --sudoloop --devel --timeupdate --batchinstall=false --pgpfetch)
+    ;;
+  *)
+    dunstify -i preferences-system-notifications-symbolic "Updates skipped" "Unsupported AUR helper: ${helper}"
+    exit 1
+    ;;
+esac
+
+if ! command -v "${cmd[0]}" >/dev/null 2>&1; then
+  dunstify -i preferences-system-notifications-symbolic "Updates failed" "AUR helper ${cmd[0]} not found"
+  exit 1
+fi
+
+"${cmd[@]}"
+status=$?
 
 # Send notification if update was successful
-if [ $? -eq 0 ]; then
+if [ $status -eq 0 ]; then
     dunstify -i preferences-system-notifications-symbolic "Updates installed" "All updates have been installed"
 else
     dunstify -i preferences-system-notifications-symbolic "Updates failed" "There was an error while installing updates"
 fi
+
+exit $status
