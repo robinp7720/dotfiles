73d83d87d7d63ee4736e045b33287476185d71d0
Robin Decker <robin@decker.cx>
2026-01-24 00:45:13 +0100

    Update polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,10 @@
-killall polybar
+#!/usr/bin/env bash
 
-polybar main -r &
-polybar secondary -r &
-polybar third -r &
+# Terminate already running bar instances
+polybar-msg cmd quit
 
+# Launch "main" bar
+echo "---" | tee -a /tmp/polybar_main.log
+polybar main -c ~/.dotfiles/polybar/config.ini 2>&1 | tee -a /tmp/polybar_main.log & disown
 
+echo "Polybar launched..."
--- polybar/scripts/current-layout.sh
+++
@@ -1,5 +0,0 @@
-desktop=$(bspc query --desktop $1:focused --desktops --names)
-layout=$(bsp-layout get $desktop)
-echo $layout
-
-
